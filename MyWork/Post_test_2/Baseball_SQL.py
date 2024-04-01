19435e8baa9966354600eecfd958839d02490eae
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-04-01 12:15:49 -0230

    Finished Baseball.sql

--- MyWork/Post_test_2/Baseball_SQL.py
+++ MyWork/Post_test_2/Baseball_SQL.py
@@ -1,6 +1,9 @@
 import sqlite3
 from dataclasses import dataclass
 
+"""
+ -- For Testing Purposes --
+
 conn = sqlite3.connect('Baseball.sqlite')
 c = conn.cursor()
 c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Player';")
@@ -22,6 +25,7 @@ conn.close()
 #         (7, 7, "Alex", "Dickerson", "LF", 586, 160),
 #         (8, 8, "Austin", "Slater", "CF", 569, 147),
 #         (9, 9, "Kevin", "Gausman", "P", 56, 2);
+"""
 
 
 @dataclass
@@ -47,26 +51,31 @@ class Player:
             return 0.0
 
     def __str__(self):
-        return f"{self.playerID}{self.batOrder}{self.fullName:40}{self.position:6}{self.atBats:>6d}{self.hits:>8d}{self.avg:>10.3f}"
+        return f"{self.playerID:5}{self.batOrder:5}{self.fullName:40}{self.position:6}{self.atBats:>6d}{self.hits:>8d}{self.avg:>10.3f}"
 
 
-@dataclass
 class Lineup:
-    __player_list: list
+    def __init__(self):
+        self.__players = []
+
+        conn = sqlite3.connect('Baseball.sqlite')
+        c = conn.cursor()
+        c.execute("select * from Player")
+        rows = c.fetchall()
+        conn.close
+
+        for row in rows:
+            if row is not None:
+                self.__players.append(row)
 
     @property
-    def count(self):
-        return len(self.__player_list)
-
-    def addPlayer(self, player: Player):
-        self.__player_list.append(player)
-
-    def removePlayer(self, number):
-        self.__player_list.pop(number-1)
+    def player_count(self):
+        return len(self.__players)
 
     def __iter__(self):
-        for player in self.__player_list:
-            yield player
+        for player in self.__players:
+            yield str(player)
+
 
 
 def title():
@@ -120,15 +129,40 @@ def get_hits(at_bats):
 
 
 def add():
-    Lineup.add_player()
+    lineup = Lineup()
+    first_name = str(input("First name: "))
+    last_name = str(input("Last name: "))
+    pos = get_pos()
+    bats = get_at_bats()
+    hits = get_hits(bats)
 
+    newid = lineup.player_count + 1
+
+    query2 = f"INSERT INTO Player VALUES ({newid}, {newid}, '{first_name}', '{last_name}', '{pos}', {bats}, {hits});"
+
+    conn = sqlite3.connect('Baseball.sqlite')
+    c = conn.cursor()
+    c.execute(query2)
+    conn.commit()
+    conn.close()
+
+    print("Player was added.")
 
 def player_list():
-    lineup = Lineup()
-    print(f"{'':3}{'Player':40}{'POS':6}{'AB':>6}{'H':>8}{'AVG':>10}")
+    print(f"{'ID':5}{'BO':5}{'Fname':12}{'Lname':12}{'POS':>6}{'AB':>8}{'H':>10}")
     print("-" * 75)
-    for player in lineup:
-        print(player)
+
+    conn = sqlite3.connect('Baseball.sqlite')
+    c = conn.cursor()
+    c.execute("SELECT * FROM Player;")
+    rows = c.fetchall()
+    conn.close()
+
+    for row in rows:
+        if row != None:
+            print(f"{row[0]:<5}{row[1]:<5}{row[2]:12}{row[3]:12}{row[4]:>6}{row[5]:>8}{row[6]:>10}")
+
+    print()
 
 
 def main():
@@ -136,23 +170,27 @@ def main():
     print("")
     menu = 0
     while menu != 6:
-        menu = int(input("\nMenu option: "))
+        menu = int(input("Menu option: "))
         if menu == 1:
             # Display Lineup
+            print()
             player_list()
         elif menu == 2:
             # Add Player
+            print()
             add()
         elif menu == 3:
             # Remove Player
-            num = input("Number of player to remove.")
-            Lineup.removePlayer(num)
+            print()
+            remove()
         elif menu == 4:
             # Update batting order
-            pass
+            print()
+            update_bat()
         elif menu == 5:
             # Update player data
-            pass
+            print()
+            update_stat()
         elif menu == 6:
             # Exit
             break
@@ -160,6 +198,68 @@ def main():
             print("Invalid entry, please try again.")
             menu = int(input("\nMenu option: "))
     print("Bye!")
+
+
+def num(check):
+    if check == 1:
+        prnt = "Enter player ID: "
+    elif check == 2:
+        prnt = "Enter new bat order number: "
+
+    while True:
+        try:
+            player_id = int(input(prnt))
+            break
+        except ValueError:
+            print("Invalid integer.")
+            continue
+
+    return player_id
+
+
+def update_bat():
+    player_id = num(1)
+    new_order = num(2)
+
+    query = f"UPDATE Player SET batOrder = {new_order} WHERE playerID = {player_id}"
+
+    conn = sqlite3.connect('Baseball.sqlite')
+    c = conn.cursor()
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
+    print("\nBat Order Updated")
+
+
+def update_stat():
+    player_id = num(1)
+    pos = get_pos()
+    bats = get_at_bats()
+    hits = get_hits(bats)
+
+    query = f"UPDATE Player SET position = '{pos}', atBats = {bats}, hits = {hits} WHERE playerID = {player_id}"
+
+    conn = sqlite3.connect('Baseball.sqlite')
+    c = conn.cursor()
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
+    print("\nPlayer Stats Updated")
+
+
+def remove():
+    player_id = num(1)
+    query = "DELETE FROM Player WHERE playerID = '{}'".format(player_id)
+
+    conn = sqlite3.connect('Baseball.sqlite')
+    c = conn.cursor()
+    c.execute(query)
+    conn.commit()
+    conn.close()
+
+    print("Player Deleted")
 
 
 if __name__ == "__main__":
